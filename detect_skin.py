fd63e852c6b581593a7fc0784c1bd38a527e28c6
hrisbh10 <hrishabh.raj4705@gmail.com>
2020-02-14 17:41:18 +0530

    Added skin detection module

---
+++ detect_skin.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import pickle
+
+cam = cv2.VideoCapture(0)
+hist = None
+
+while True:
+    _,frame = cam.read()
+    frame = cv2.flip(frame,1)
+    cv2.rectangle(frame,(422,80),(594,418),(0,0,255),2)
+
+    roi = frame[80:418,422:594]
+    hsv = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
+    cv2.imshow('frame',frame)
+
+    k = cv2.waitKey(5)
+
+    if k == ord('s'):
+        hist = cv2.calcHist([hsv],[0,1],None,[180,256],[0,180,0,256])
+        cv2.normalize(hist,hist,0,255,cv2.NORM_MINMAX)
+        break
+
+cam.release()
+cv2.destroyAllWindows()
+
+with open("skin","wb") as file:
+    pickle.dump(hist,file)
